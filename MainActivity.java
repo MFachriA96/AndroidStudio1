8537e20f019db0c5d1bf682ff7b5dedbc440eb73
MFachriA96 <149947273+MFachriA96@users.noreply.github.com>
2025-02-15 21:18:24 +0700

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.tugas1;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.TextView;
+import android.graphics.Color;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
